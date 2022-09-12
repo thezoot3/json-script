f547ad60d0dd7192e470c7cb143cadeba971d04e
thezoot3 <thezoot3@gmail.com>
2022-09-12 17:37:25 +0900

    임포트 위치 수정

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { scriptConfig as ScriptConfig } from './store/ScriptLang.jsx';
-import { scriptLoader as ScriptLoader } from './store/ScriptLoader';
-import Script from './component/Script'
-import Placeholder from "./component/Placeholder";
-export { ScriptConfig, ScriptLoader, Script, Placeholder }
+import { ScriptLang } from './src/context/ScriptLang.jsx';
+import { ScriptLoader } from './src/context/ScriptLoader';
+import Script from './src/component/Script'
+export { ScriptLang, ScriptLoader, Script }

9ff8208d0255e8f3c61a1448646c78a6ca5846bd
thezoot3 <thezoot3@gmail.com>
2022-09-16 19:25:55 +0900

    기반 파일 변경

---
+++ src/component/Placeholder.js
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+
+/**
+ *
+ * @param name
+ * @param value
+ * @param disabled
+ * @returns {null}
+ */
+function Placeholder({name, value, disabled = false}) {
+    return disabled ? null : {[name]: value}
+}
+Placeholder.propTypes = {
+    name: PropTypes.string.isRequired,
+    value : PropTypes.any.isRequired,
+    disabled: PropTypes.bool,
+}
+export default Placeholder

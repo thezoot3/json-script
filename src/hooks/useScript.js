9b61213fb896bcd1016e0775c4d3af2dc63a77ad
thezoot3 <thezoot3@gmail.com>
2022-09-21 17:26:55 +0900

    오류 수정

--- src/hooks/useScript.js
+++ src/hooks/useScript.js
@@ -1,9 +1,15 @@
 import Script from "../component/Script";
 import {createSelectorHook} from "react-redux";
+import {useEffect, useState} from "react";
 
 function useScript(loaderContext) {
-    const selector = createSelectorHook(loaderContext)
-    return ({name, placeholder, children}) => {
+    const [selector, setSelector] = useState(() => {
+        return createSelectorHook(loaderContext);
+    })
+    useEffect(() => {
+        setSelector(() => {return createSelectorHook(loaderContext)})
+    }, [loaderContext])
+    return ({name, placeholder = {}, children = []}) => {
         return (
             <Script name={name} placeholder={placeholder} scriptSelector={selector}>
                 {children.constructor === Array ? children : [children]}

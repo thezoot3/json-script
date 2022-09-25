71924d040e3de2ca1e840c78c9267b91ace9687b
thezoot3 <thezoot3@gmail.com>
2022-09-25 17:16:05 +0900

    EsLint 테스트 오류 수정

--- Lint.js
+++ Lint.js
@@ -1,17 +1,11 @@
 const { ESLint } = require("eslint");
 (async function main() {
-    lint().then(async ({result, text}) => {
-        if(ESLint.getErrorResults(result).length > 0) {
-            throw new Error("EsLint failed, Some Syntax are violated ESLint Rules\n" + text)
-        }
-    }).catch(error => {
-        throw error;
-    })
-})()
-async function lint() {
     const eslint = new ESLint();
-    const results = await eslint.lintFiles(["src/**/*"]);
+    const results = await eslint.lintFiles(["src/**/*.js"]);
     const formatter = await eslint.loadFormatter("stylish");
     const resultText = formatter.format(results);
-    return {results, resultText}
-}
+    if(ESLint.getErrorResults(results).length > 0) {
+        throw new Error("EsLint failed, Some Syntax are violated ESLint Rules\n" + resultText)
+    }
+})()
+

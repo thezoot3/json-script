1be31687683675bca57977cb663e17e45909b124
thezoot3 <thezoot3@gmail.com>
2022-09-25 17:21:56 +0900

    로직 변경

--- src/hooks/useScript.js
+++ src/hooks/useScript.js
@@ -1,6 +1,7 @@
 import Script from "../component/Script";
 import {createSelectorHook} from "react-redux";
 import {useEffect, useState} from "react";
+import PropTypes from "prop-types";
 
 function useScript(loaderContext) {
     const [selector, setSelector] = useState(() => {
@@ -9,12 +10,18 @@ function useScript(loaderContext) {
     useEffect(() => {
         setSelector(() => {return createSelectorHook(loaderContext)})
     }, [loaderContext])
-    return ({name, placeholder = {}, children = []}) => {
+    function script({name, placeholder = {}, children = []}) {
         return (
             <Script name={name} placeholder={placeholder} scriptSelector={selector}>
                 {children.constructor === Array ? children : [children]}
             </Script>
         )
     }
+    script.propTypes = {
+        name: PropTypes.string.isRequired,
+        placeholder: PropTypes.object,
+        children: PropTypes.object
+    }
+    return script;
 }
 export default useScript;

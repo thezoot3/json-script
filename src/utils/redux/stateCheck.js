85f9075481fba7a05b56f8bf74858a39acc9fed3
thezoot3 <thezoot3@gmail.com>
2022-09-25 17:21:48 +0900

    ESLint 수정

--- src/utils/redux/stateCheck.js
+++ src/utils/redux/stateCheck.js
@@ -1,7 +1,7 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default function(state, defaultState) {
+function stateCheck(state, defaultState) {
     return Object.keys(state).every(i => {
         if(i === "isReady") return true;
         return (state[i] || defaultState[i] !== state[i]);
     })
 }
+export default stateCheck;

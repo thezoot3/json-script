3ba3de0562d86a6d01803fefd72ebee8f5473a97
thezoot3 <thezoot3@gmail.com>
2022-09-27 19:26:30 +0900

    이름 변경으로 인한 임포트 수정

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import ScriptConfig from './src/store/ScriptConfig';
 import ScriptLoader from './src/store/ScriptLoader';
 import Script from './src/component/Script'
 import Placeholder from "./src/component/Placeholder";
-import useScript from "./src/hooks/useScript";
-export { ScriptConfig, ScriptLoader, Script, Placeholder, useScript }
+import useScriptFactory from "./src/hooks/useScriptFactory";
+export { ScriptConfig, ScriptLoader, Script, Placeholder, useScriptFactory }
